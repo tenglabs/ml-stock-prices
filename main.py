55ecdbdeca29fcf8107c1cac9e5350370a4ea04a
tenglabs <tengoneotechnologies@gmail.com>
2021-06-10 01:46:46 +0300

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy_as np 
+import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd 
 import pandas_datareader as web 
@@ -31,4 +31,35 @@ for x in range(prediction_days, len(scaled_data)):
     y_train.append(scaled_data[x, 0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train,(x_train[0]))
+x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1], 1))
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimzer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model.reshape(-1,1)
+model_inputs = scaler.transfor(model_inputs)
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs)
